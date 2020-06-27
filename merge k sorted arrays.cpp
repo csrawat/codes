0adc97152580736ec0fe0a1642cf0e836f59616e
Chandra Shekhar Rawat <cs215100@gmail.com>
2020-06-27 21:47:44 +0530

    added comments

--- merge k sorted arrays.cpp
+++ merge k sorted arrays.cpp
@@ -1,12 +1,13 @@
 #include<bits/stdc++.h>
-#define ppi pair<int,pair<int ,int> >
+#define ppi pair<int,pair<int ,int> > // stores { element , { index of array , index of element in array } }
 using namespace std;
 
 vector<int> mergeSort(vector<vector<int> > arr)
 {
-    vector<int> res;
-    priority_queue<ppi,vector<ppi>,greater<ppi> > pq;
+    vector<int> res;    // stores final result
+    priority_queue<ppi,vector<ppi>,greater<ppi> > pq;   // creating min_heap ( root of heap contains min element )
 
+    // insert first element of each array into the pq
     for(int i=0;i<arr.size();i++)
     {
         pq.push({arr[i][0],{i,0}});
@@ -17,13 +18,13 @@ vector<int> mergeSort(vector<vector<int> > arr)
         ppi curr = pq.top();
         pq.pop();
 
-        int data = curr.first;
-        int i = curr.second.first;
-        int j = curr.second.second;
+        int data = curr.first;          // element
+        int i = curr.second.first;      // index of array
+        int j = curr.second.second;     // index of element in array
 
         res.push_back(data);
 
-        if(j+1<arr[i].size())
+        if(j+1<arr[i].size())   // checking if there are more elements present in the array to process 
         {
             pq.push({arr[i][j+1],{i,j+1}});
         }
