5bf07c67f72a4094296279b3be79ffa20bd66ea2
Chandra Shekhar Rawat <cs215100@gmail.com>
2020-06-28 08:15:31 +0530

    adding comments

--- kadane's algorithm.cpp
+++ kadane's algorithm.cpp
@@ -1,19 +1,21 @@
+//modified kadane's algorithm. works for both +ve and -ve values and also for all -ve values.
 #include<bits/stdc++.h>
 
 using namespace std;
 
 vector<int> solve(vector<int> a)
 {
-    vector<int> res(3,-1);
-    int msf = INT_MIN , meh = 0;
-    int n = 0, m = 0;
+    vector<int> res(3,-1);          // stores max sum, start index, end index
+    int msf = INT_MIN , meh = 0;    // msf - max so far, meh - max ending here
+    int n = 0, m = 0;               // n - start index, m = end index
+    
     for(int i=0;i<a.size();i++)
     {
         meh += a[i];
         if(a[i]>meh)
         {
             meh = a[i];
-            if(msf<meh) n = i;
+            if(msf<meh) n = i;      // dry run {-1,-2,-3,-4} for understanding this check.
         }
 
         if(msf<meh)
