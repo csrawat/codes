cf352ec5bc574589289165f14a863bc41f9df3a8
Chandra Shekhar Rawat <cs215100@gmail.com>
2020-06-28 11:41:11 +0530

    Update kadane's algorithm.cpp

--- kadane's algorithm.cpp
+++ kadane's algorithm.cpp
@@ -1,4 +1,4 @@
-//modified kadane's algorithm. works for both +ve and -ve values and also for all -ve values.
+// modified kadane's algorithm. works for both +ve and -ve values and also for all -ve values.
 
 #include<bits/stdc++.h>
 
@@ -6,8 +6,10 @@ using namespace std;
 
 vector<int> solve(vector<int> arr)
 {
-    vector<int> res(3,-1);
+    vector<int> res(3,-1);      // stores max_sum, start_index, end_index
+    
     int max = arr[0], start = 0, end = 0, mstart = 0, mend = 0, currmax = arr[0];
+    
         for (int i = 1; i < arr.size(); i++) {
             if (  currmax < 0 ) {
                 currmax = arr[i];
